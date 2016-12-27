21e99db9ab617813faffa88f480b05b3c8a472f2
Wator Vapor <watorvapor@gmail.com>
2016-12-28 00:33:03 +0900

    Update wrapper.fuse.cpp

--- fsmain/fuse/wrapper.fuse.cpp
+++ fsmain/fuse/wrapper.fuse.cpp
@@ -6,4 +6,7 @@ static int wrapp_getattr (const char *, struct stat *, struct fuse_file_info *fi
 static const struct fuse_operations smcfs_opertaions = {
   &wrapp_getattr,
 };
-//smcfs_opertaions.getattr = &wrapp_getattr;
+
+
+static int wrapp_getattr (const char *, struct stat *, struct fuse_file_info *fi) {
+}
